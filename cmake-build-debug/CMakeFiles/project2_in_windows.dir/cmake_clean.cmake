ba3a21e7c33ebc0fafd9cb62712d50993d8d2050
zhouquan <365712127@qq.com>
2022-01-19 13:10:07 +0800

    first

---
+++ cmake-build-debug/CMakeFiles/project2_in_windows.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project2_in_windows.dir/main.c.obj"
+  "libproject2_in_windows.dll.a"
+  "project2_in_windows.exe"
+  "project2_in_windows.exe.manifest"
+  "project2_in_windows.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project2_in_windows.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/project2_in_windows.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for project2_in_windows.
+# This may be replaced when dependencies are built.
